79f16e2b6ecfb0ed34e4b25c7e5e095cbc9c9738
IsmaelHdz <imismaelhdz@gmail.com>
2023-02-25 23:33:41 -0600

    Initial  project structure and adding H2 database configuration

---
+++ src/test/java/com/example/srfapi/SpringbootRestfulApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
