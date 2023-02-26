687084e733a264ccd5d4a8a14932ef54c475bfc6
IsmaelHdz <imismaelhdz@gmail.com>
2023-02-26 02:15:55 -0600

    Working on Bean CRUD endpoints

--- src/main/java/com/example/srfapi/service/BeanService.java
+++ src/main/java/com/example/srfapi/service/BeanService.java
@@ -1,4 +1,14 @@
 package com.example.srfapi.service;
 
+import com.example.srfapi.model.Bean;
+
+import java.util.List;
+
 public interface BeanService {
+
+    List<Bean> getAllBeans();
+    Bean getBeanById(Long id);
+    Bean saveBean(Bean bean);
+    Bean deleteBean(Bean bean);
+    Bean updateBwan(Long id , Bean bean);
 }
--- src/main/java/com/example/srfapi/service/BeanServiceImpl.java
+++ src/main/java/com/example/srfapi/service/BeanServiceImpl.java
@@ -1,4 +1,36 @@
 package com.example.srfapi.service;
 
-public class BeanServiceImpl {
+import com.example.srfapi.model.Bean;
+import com.example.srfapi.repository.BeanRepository;
+
+import java.util.List;
+
+public class BeanServiceImpl implements BeanService{
+    BeanRepository beanRepository;
+
+
+    @Override
+    public List<Bean> getAllBeans() {
+        return null;
+    }
+
+    @Override
+    public Bean getBeanById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Bean saveBean(Bean bean) {
+        return null;
+    }
+
+    @Override
+    public Bean deleteBean(Bean bean) {
+        return null;
+    }
+
+    @Override
+    public Bean updateBwan(Long id, Bean bean) {
+        return null;
+    }
 }
