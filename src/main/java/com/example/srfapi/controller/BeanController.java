999fc26804c4fcfb01e4229879587e0d6b796410
IsmaelHdz <imismaelhdz@gmail.com>
2023-02-28 14:40:39 -0600

    Adding more endpoints  to Bean

--- src/main/java/com/example/srfapi/controller/BeanController.java
+++ src/main/java/com/example/srfapi/controller/BeanController.java
@@ -5,11 +5,14 @@ import com.example.srfapi.service.BeanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class BeanController {
     @Autowired
     BeanService beanService;
-
+    @GetMapping("/beans")
+    public List<Bean> getBeans(){ return beanService.getAllBeans();}
     @PostMapping("/bean")
     public Bean saveBean(@RequestBody Bean bean){
         return beanService.saveBean(bean);}
@@ -19,4 +22,7 @@ public class BeanController {
 
     @DeleteMapping("/bean/{id}")
     public String deleteBean(@PathVariable("id") Long id){return beanService.deleteBean(id);}
+
+    @GetMapping("/bean/count")
+    public Long countBean(){ return beanService.countBeans();}
 }
--- src/main/java/com/example/srfapi/service/BeanServiceImpl.java
+++ src/main/java/com/example/srfapi/service/BeanServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.srfapi.repository.BeanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.beans.Beans;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,21 +13,17 @@ import java.util.Optional;
 public class BeanServiceImpl implements BeanService{
     @Autowired
     BeanRepository beanRepository;
-
-
     @Override
     public List<Bean> getAllBeans() {
         List<Bean> beans = new ArrayList<Bean>();
         beanRepository.findAll().forEach(beans::add);
         return beans;
     }
-
     @Override
     public Bean getBeanById(Long id) {
         Optional<Bean> bean = beanRepository.findById(id);
         return bean.orElse(null);
     }
-
     @Override
     public Bean saveBean(Bean bean) {
         return beanRepository.save(bean);
@@ -39,9 +34,11 @@ public class BeanServiceImpl implements BeanService{
         beanRepository.deleteById(id);
         return "id"+ id + "was delete successfully";
     }
-
     @Override
     public Bean updateBean(Long id, Bean bean) {
         return null;
     }
+
+    @Override
+    public Long countBeans() {return beanRepository.count();}
 }
